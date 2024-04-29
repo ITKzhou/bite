bcc38f425669b4fcb811d82b2c3a6c8faa026b5c
ITKzhou <1546626108@qq.com>
2024-04-29 19:48:50 +0800

    完成函数的课程代码

---
+++ Codes/函数.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//计算某年某月有多少天？
+
+//#include <stdbool.h>
+//
+////声明函数
+//int get_days(int, int);
+//bool is_leap_year(int);
+//
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	scanf("%d %d", &year, &month);
+//	printf("%d", get_days(year,month));
+//	return 0;
+//}
+//
+////得到天数
+//int get_days(int y, int m)
+//{
+//	int days[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+//	//判断是闰年
+//	if (is_leap_year(y) && m == 2)
+//	{
+//		return ++days[m];
+//	}
+//	else
+//		return days[m];
+//}
+////判断是闰年函数
+//bool is_leap_year(int year)
+//{
+//	if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+//		return true;
+//	}
+//	else {
+//		return false;
+//	}
+//}
+
+
