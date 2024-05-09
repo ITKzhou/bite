f5c57148c87e32d898bf9d8c5fdbe9993ef90d91
ITKzhou <1546626108@qq.com>
2024-05-09 23:58:55 +0800

    完成指针进阶(3)课程

--- Codes/指针进阶.c
+++ Codes/指针进阶.c
@@ -361,6 +361,124 @@ void menu()
 
 
 
+//使用【冒泡排序】，模拟实现一个排序函数bubble_sort()，可以排序任意类型的数据
+
+#include <string.h>
+
+//void swap(char* buf1, char* buf2, size_t size)
+//{
+//	size_t i = 0;
+//	for (i = 0; i < size; i++)
+//	{
+//		char tmp = *buf1;
+//		*buf1 = *buf2;
+//		*buf2 = tmp;
+//		buf1++;
+//		buf2++;
+//	}
+//}
+//
+//void bubble_sort(void* base, size_t num, size_t size, int (*cmp)(const void* p1, const void* p2))
+//{
+//	size_t i = 0;
+//	for (i = 0; i < num - 1; i++)
+//	{
+//		size_t j = 0;
+//		
+//		for (j = 0; j < num - 1 - i; j++)
+//		{
+//			//if(arr[j] > arr[j + 1])
+//			if (cmp((char*)base + j * size, (char*)base + (j + 1) * size) > 0)
+//			{
+//				//每个字节都交换
+//				swap((char*)base + j * size, (char*)base + (j + 1) * size, size);
+//			}
+//		}	
+//	}
+//}
+//
+//void Print_arr(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//
+//int cmp_int(const void* p1, const void* p2)
+//{
+//	//升序
+//	//return *(int*)p1 - *(int*)p2;
+//	
+//	//降序
+//	return *(int*)p2 - *(int*)p1;
+//
+//}
+//
+//void test_int_sort()
+//{
+//	int arr[10] = { 2,5,1,6,3,0,4,9,8,7 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//
+//	Print_arr(arr, sz);
+//	bubble_sort(arr, sz, sizeof(arr[0]), cmp_int);
+//	Print_arr(arr, sz);
+//}
+//
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//void Print_s(struct Stu* ps, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%s, %d\n", (ps + i)->name, (ps + i)->age);
+//	}
+//	printf("\n");
+//}
+//
+//int cmp_struct_name(const void* p1, const void* p2)
+//{
+//	//return strcmp((*(struct Stu*)p1).name, (*(struct Stu*)p2).name);
+//	return strcmp(((struct Stu*)p1)->name, ((struct Stu*)p2)->name);
+//}
+//
+//int cmp_struct_age(const void* p1, const void* p2)
+//{
+//	return ((struct Stu*)p1)->age - ((struct Stu*)p2)->age;
+//}
+//
+//void test_struct_sort()
+//{
+//	struct Stu s[] = { {"zhangsan",18},{"lisi",23},{"wnagwu",20} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//
+//	Print_s(s,sz);
+//	bubble_sort(s, sz, sizeof(s[0]), cmp_struct_name);
+//	Print_s(s, sz);
+//	bubble_sort(s, sz, sizeof(s[0]), cmp_struct_age);
+//	Print_s(s, sz);
+//}
+//
+//int main()
+//{
+//	//排序整型数据
+//	test_int_sort();
+//
+//	//排序结构体数据
+//	test_struct_sort();
+//
+//	return 0;
+//}
+
+
 
 
 
