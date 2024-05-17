3f837961cc3f1e0a53d9a8ee9e845411fadaaf07
ITKzhou <1546626108@qq.com>
2024-05-18 01:46:42 +0800

    完成时间复杂度(2)课程与笔记

--- DataStructure/Complexity.c
+++ DataStructure/Complexity.c
@@ -57,15 +57,14 @@
 ///————————————————————————————————————————————————————————————————————————————            
 ///@Codeing :
 ///                                                                           
-///    数组nums包含从0到n的所有整数，但其中缺了一个。请编写代码找出那个缺失的整数                                                                                                                                                    
+///    数组nums包含从0到n的所有整数，但其中缺了一个。请编写代码找出那个缺失的整数  
+///                异或操作符     找单身狗题型                                                                                                                                   
 ///****************************************************************************          
 
-//时间复杂度：O(N)
-//
 //int missingNumber(int* nums, int numsSize) {
 //	int i = 0;
 //	int sum = 0;
-//	for (; i < numsSize; i++) {//避免等于numsSize,会导致越界访问
+//	for (; i < numsSize; i++) {//不能等于numsSize,避免导致越界访问
 //		sum ^= i;//遍历数组，下标异或累积在，再与数组元素异或累积
 //		sum ^= nums[i];
 //	}
@@ -78,13 +77,173 @@
 //	return 0;
 //}
 
+//时间复杂度：O(N)
+//空间复杂度：O(1)
+
+///————————————————————————————————————————————————————————————————————————————  
+
+
+///@Time    :
+
+///@Problem : 轮转数组
+///                                                                           
+///	https://leetcode.cn/problems/rotate-array/description/ 
+/// 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数 
+/// 使用额外的数组                                                                      
+///————————————————————————————————————————————————————————————————————————————            
+///@Codeing :
+///                                                                           
+///         数组翻转，整体翻转，再前面翻转，再后面翻转                                                                                                                                             
+///****************************************************************************
+          
+//void swap(int* a, int* b)//交换两个元素
+//{
+//	int t = *a;
+//	*a = *b;
+//	*b = t;
+//}
+//void reverse(int* nums, int start, int end)//控制轮转开始结束的位置
+//{
+//	while (start < end)
+//	{
+//		swap(&nums[start], &nums[end]);
+//		++start;
+//		--end;
+//	}
+//}
+//
+//void rotate(int* nums, int numsSize, int k)
+//{
+//	k %= numsSize;//确保轮转的元素个数不大于数组长度
+//	//对数组轮转，传参开始和结束的下标
+//	reverse(nums, 0, numsSize - 1);//轮转整个数组
+//	reverse(nums, 0, k - 1);//轮转前面部分
+//	reverse(nums, k, numsSize - 1);//轮转后面部分
+//
+//}
+//
+//int main()
+//{
+//	int k = 0;
+//	int arr[] = { 1,2,3,4,5,6,7 };
+//	scanf("%d", &k);
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	rotate(arr, sz, k);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);//k=3	[5,6,7,1,2,3,4]
+//	}
+//	return 0;
+//}
+
+
+/// @Codeing  :  
+///                                                                           
+///         使用额外的数组                                                                                                                                               
+///****************************************************************************          
+
+//void rotate(int* nums, int numsSize, int k)
+//{
+//	int arr[7] = { 0 };
+//
+//	//nums 前面的 numsSize - k 个元素从 arr数组下标为 k 的位置开始存放
+//	int j = k;
+//	for (int i = 0; i < numsSize - k; ++i)//[1,2,3,4,5,6,7]
+//	{
+//		arr[j++] = nums[i];
+//	}
+//	//nums 后面的 k 个元素起始下标[numsSize - k]从 arr[0] 的位置开始存放
+//	j = 0;
+//	for (int i = numsSize - k; i < numsSize; ++i)
+//	{
+//		arr[j++] = nums[i];
+//	}
+//	for (int i = 0; i < numsSize; ++i)
+//	{
+//		nums[i] = arr[i];
+//	}
+// }
+//
+//int main()
+//{
+//	int k = 0;
+//	int arr[7] = { 1,2,3,4,5,6,7 };
+//	scanf("%d", &k);
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	rotate(arr, sz, k);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);//k=3	[5,6,7,1,2,3,4]
+//	}
+//	return 0;
+//}
+
 ///————————————————————————————————————————————————————————————————————————————  
 
 
 
+///@Time    :
+
+///@Problem : 移除元素
+///                                                                           
+///  https://leetcode.cn/problems/remove-element/description/                                                                          
+///————————————————————————————————————————————————————————————————————————————            
+///@Codeing :
+///                                                                           
+///     给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。
+/// 元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数                                                                                                                                                   
+///****************************************************************************          
 
 
 
 
 
+
+
+///————————————————————————————————————————————————————————————————————————————  
+
+//输入：nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3
+//输出：[1, 2, 2, 3, 5, 6]
+//解释：需要合并[1, 2, 3] 和[2, 5, 6] 。
+//合并结果是[1, 2, 2, 3, 5, 6] ，其中斜体加粗标注的为 nums1 中的元素。
+
+//
+//void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+//    int p1 = 0, p2 = 0;
+//    int sorted[6] = { 0 };
+//    int cur;
+//    while (p1 < m || p2 < n) {
+//        if (p1 == m) {
+//            cur = nums2[p2++];
+//        }
+//        else if (p2 == n) {
+//            cur = nums1[p1++];
+//        }
+//        else if (nums1[p1] < nums2[p2]) {
+//            cur = nums1[p1++];
+//        }
+//        else {
+//            cur = nums2[p2++];
+//        }
+//        sorted[p1 + p2 - 1] = cur;
+//    }
+//    for (int i = 0; i != m + n; ++i) {
+//        nums1[i] = sorted[i];
+//    }
+//}
+//
+//int main()
+//{
+//    int nums1[] = {1, 2, 3, 0, 0, 0};
+//    int nums2[] = {2, 5, 6};
+//    int m = 3;
+//    int n = 3;
+//    int nums1Size = sizeof(nums1) / sizeof(nums1[0]);
+//    int nums2Size = sizeof(nums2) / sizeof(nums2[0]);
+//    merge(nums1, nums1Size, m,  nums2, nums2Size,  n);
+//    for (int i = 0; i != m + n; ++i) {
+//        printf("%d ", nums1[i]);
+//    }
+//    return 0;
+//}
 
